3ae820a4d4d13c5223a2096d7b39c0ea2107633a
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2020-11-30 11:28:41 +0100

    Update template :arrows_clockwise:
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- template/src/test/Main.cpp
+++ template/src/test/Main.cpp
@@ -1,43 +1,76 @@
-//
-// Created by vincent on 9/14/20.
-//
+/**
+ * Segment tree data structure implementation
+ * Copyright (C) 2020  Vincenzo Palazzo vincenzopalazzodev@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 #include <cstdlib>
 #include <vector>
-#include <fstream>
-#include "../core/SolTest.h"
-#include "../Utils.cpp"
-#include "TestTool.cpp"
+#include "TestTool.hpp"
+#include "../core/RangeMinimumQuery.hpp"
 
 using namespace std;
-using namespace cpstl;
 
-void TEST_CASE_ONE()
+void TEST_CASE_ONE_SEGMENT_TREE()
 {
-    vector<int> inputs = {20, 90, 40, 90};
-    int maximum_house = get_maximum_number_of_house<int, int>(inputs, 100);
-    assert_equal("TEST_CASE_ONE", 2, maximum_house);
+    vector<int> inputs = {18, 17, 13, 19, 15, 11, 20};
+    cpstl::cp_log(LOG, inputs);
+    vector<Query<int>> queries;
+    queries.emplace_back(false, 5, 7);
+    queries.emplace_back(false, 1, 3);
+    queries.emplace_back(true, 6, 12);
+    queries.emplace_back(false, 5, 7);
+    queries.emplace_back(false, 3, 4);
+    queries.emplace_back(false, 1, 2);
+    queries.emplace_back(false, 1, 7);
+    queries.emplace_back(false, 7, 7);
+    auto segment_tree = cpstl::SegmentTree<int>(inputs);
+    auto result = range_minimum_query_segment_tree(segment_tree, queries);
+    cpstl::assert_equal("TEST_CASE_ONE_SEGMENT_TREE", {11, 13, 12, 13, 17, 12, 20}, result);
 }
 
-void TEST_CASE_TWO()
+void TEST_CASE_TWO_SEGMENT_TREE()
 {
-    vector<int> inputs = {30, 30, 10, 10};
-    int maximum_house = get_maximum_number_of_house<int, int>(inputs, 50);
-    assert_equal("TEST_CASE_TWO", 3, maximum_house);
+    vector<int> inputs = {1, 5, 2, 4, 3};
+    cpstl::cp_log(LOG, inputs);
+    vector<Query<int>> queries;
+    queries.emplace_back(false, 1, 5);
+    queries.emplace_back(false, 1, 3);
+    queries.emplace_back(false, 3, 5);
+    queries.emplace_back(true, 3, 6);
+    queries.emplace_back(false, 1, 5);
+    auto segment_tree = cpstl::SegmentTree<int>(inputs);
+    auto result = range_minimum_query_segment_tree(segment_tree, queries);
+    cpstl::assert_equal("TEST_CASE_TWO_SEGMENT_TREE", {1, 1, 2, 1}, result);
 }
 
-
-void TEST_CASE_TREE()
+void TEST_CASE_ONE_NAIVE()
 {
-    vector<int> inputs = {999, 999, 999};
-    int maximum_house = get_maximum_number_of_house<int, int>(inputs, 300);
-    assert_equal("TEST_CASE_TREE", 0, maximum_house);
+    vector<int> inputs = {18, 17, 13, 19, 15, 11, 20};
+    cpstl::print_vector(inputs);
+    vector<Query<int>> queries;
+    queries.emplace_back(false, 4, 6);
+    queries.emplace_back(false, 1, 3);
+    auto result = range_minimum_query_naive(inputs, queries);
+    cpstl::assert_equal("TEST_CASE_ONE_NAIVE", {11, 13}, result);
 }
 
 int main()
 {
-    TEST_CASE_ONE();
-    TEST_CASE_TWO();
-    TEST_CASE_TREE();
-
+    TEST_CASE_ONE_SEGMENT_TREE();
+    TEST_CASE_ONE_NAIVE();
+    TEST_CASE_TWO_SEGMENT_TREE();
     return EXIT_SUCCESS;
 }
