cab70213c362360aaabdd64da590d320c43e4963
Vincenzo Palazzo <vincenzopalazzo1996@gmail.com>
2021-01-20 14:28:25 +0100

    Completed :heavy_check_mark:
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- PartitionEqualSubsetSum/src/MainSite.cpp
+++ PartitionEqualSubsetSum/src/MainSite.cpp
@@ -19,22 +19,77 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-#include <iostream>
-#include <vector>
+// { Driver Code Starts
+// Initial Template for C++
 
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int N, Q;
-    scanf("%d", &N);
-    scanf("%d", &Q);
+ // } Driver Code Ends
 
-    std::vector<int> inputs;
-    inputs.reserve(N);
-    //Read the array
-    for (std::size_t t = 0; t < N; t++) {
-        int value;
-        scanf("%d", &value);
-        inputs.push_back(value);
+
+// User function Template for C++
+
+class Solution{
+public:
+    int equalPartition(int n, int arr[])
+    {
+       int sum = 0;
+        int i, j;
+
+        // Calculate sum of all elements
+        for (i = 0; i < n; i++)
+            sum += arr[i];
+
+        if (sum % 2 != 0)
+            return false;
+
+        bool part[sum / 2 + 1];
+
+        // Initialze the part array
+        // as 0
+        for (i = 0; i <= sum / 2; i++) {
+            part[i] = 0;
+        }
+
+        // Fill the partition table in bottom up manner
+
+        for (i = 0; i < n; i++) {
+            // the element to be included
+            // in the sum cannot be
+            // greater than the sum
+            for (j = sum / 2; j >= arr[i];
+                 j--) { // check if sum - arr[i]
+                // could be formed
+                // from a subset
+                // using elements
+                // before index i
+                if (part[j - arr[i]] == 1 || j == arr[i])
+                    part[j] = 1;
+            }
+        }
+
+        return part[sum / 2];
     }
-}
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+
+        Solution ob;
+        if(ob.equalPartition(N, arr))
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+    return 0;
+}  // } Driver Code Ends
