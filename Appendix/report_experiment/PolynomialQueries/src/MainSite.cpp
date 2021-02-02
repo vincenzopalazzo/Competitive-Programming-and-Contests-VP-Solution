966a5a5dee60eab268b4b7e39ca579c7e22b28b7
Vincenzo Palazzo <vincenzopalazzo1996@gmail.com>
2021-02-02 14:16:02 +0100

    Working in progress :construction:
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ Appendix/report_experiment/PolynomialQueries/src/MainSite.cpp
@@ -0,0 +1,286 @@
+/**
+ * Competitive-Programming-and-Contests-VP-Solution a collection of
+ * code with an engineering approach to solve the problem.
+ * https://github.com/vincenzopalazzo/Competitive-Programming-and-Contests-VP-Solution
+ *
+ * Copyright (C) 2020-2021  Vincenzo Palazzo vincenzopalazzodev@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+namespace cpstl {
+/**
+ * Segment tree data structure implementation
+ * Copyright (C) 2020-2021  Vincenzo Palazzo vincenzopalazzodev@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+template <class T>
+class LazySegmentTree {
+ protected:
+  std::vector<T> &origin;
+  std::vector<T> structure;
+  std::vector<T> lazy;
+
+  /**
+   * This function is used build the segment tree with a binary heap
+   * implementation, and it use the origin array stored in the class.
+   * @tparam T is the type of structure, usually it is a int or another numeric
+   * type
+   * @param left_index the left index of the range
+   * @param right_index the right index of the range
+   */
+  void build_structure(std::size_t left_index, std::size_t right_index) {
+    build_structure_procedure(1, left_index, right_index - 1);
+  }
+  /**
+   * This is the sub procedure that help the build_structure procedure to make
+   * the logic inside the segment tree this version store inside the node of
+   * Segment Tree the value and not the index of the value
+   * @tparam T is the type of structure, usually it is a int or another numeric
+   * type
+   * @param start_index
+   * @param left_index
+   * @param right_index
+   */
+  void build_structure_procedure(std::size_t start_index,
+                                 std::size_t left_index,
+                                 std::size_t right_index) {
+    if (left_index == right_index) {
+      // Leaf node will have a single element
+      structure[start_index] = origin[left_index];
+      return;
+    }
+    auto middle_point = (left_index + right_index) / 2;
+    auto left_child = left_child_index(start_index);
+    auto right_child = right_child_index(start_index);
+    build_structure_procedure(left_child, left_index, middle_point);
+    build_structure_procedure(right_child, middle_point + 1, right_index);
+    // Internal node will have the sum of both of its children
+    auto segment_left = structure[left_child];
+    auto segment_right = structure[right_child];
+    structure[start_index] = segment_left + segment_right;
+  }
+
+  T range_query_subroutine(std::size_t start_index, std::size_t left_index,
+                           std::size_t right_index, std::size_t query_left,
+                           std::size_t query_right) {
+    propagate(start_index, left_index, right_index);
+    // outside the range
+    if (query_left > query_right) return 0;
+    // range represented by a node is completely inside the given range
+    if (left_index >= query_left && right_index <= query_right)
+      return structure[start_index];
+    // range represented by a node is partially inside and partially outside the
+    // given range
+    auto middle_point = (left_index + right_index) / 2;
+    auto left_child = left_child_index(start_index);
+    auto right_child = right_child_index(start_index);
+    auto left_segment =
+        range_query_subroutine(left_child, left_index, middle_point, query_left,
+                               std::min(middle_point, query_right));
+    auto right_segment = range_query_subroutine(
+        right_child, middle_point + 1, right_index,
+        std::max(query_left, middle_point + 1), query_right);
+    return left_segment + right_segment;
+  }
+
+  /**
+   * Perform the real update of the update_range operation,
+   * @param start_index: Start index, at the starting point this is 1
+   * @param left_index: At the starting point it is the first position of the
+   * original array, so 0.
+   * @param right_index: At the starting point it is the last position of the
+   * original array, so size - 1
+   * @param from: The start index where start to update original array
+   * @param to: The end index where stop to update original array
+   * @param new_val: The value to sum to each position of the range in the
+   * original array.
+   */
+  void update_range_subroutine(std::size_t start_index, std::size_t left_index,
+                               std::size_t right_index, std::size_t from,
+                               std::size_t to, T new_val) {
+    propagate(start_index, left_index, right_index);
+    if (from > to) return;
+    if ((left_index >= from) && (right_index <= to)) {
+      lazy[start_index] = new_val;
+      propagate(start_index, left_index, right_index);
+    } else {
+      auto middle_point = (left_index + right_index) / 2;
+      auto left_child = left_child_index(start_index);
+      auto right_child = right_child_index(start_index);
+      update_range_subroutine(left_child, left_index, middle_point, from,
+                              std::min(middle_point, to), new_val);
+      update_range_subroutine(right_child, middle_point + 1, right_index,
+                              std::max(from, middle_point + 1), to, new_val);
+      auto left_subtree = (lazy[left_child] != -1)
+                          ? (lazy[left_child] + structure[left_child])
+                          : structure[left_child];
+      auto right_subtree = (lazy[right_child] != -1)
+                           ? (lazy[right_child] + structure[right_child])
+                           : structure[right_child];
+      structure[start_index] = left_subtree + right_subtree;
+    }
+  }
+
+  void propagate(std::size_t start_index, std::size_t left_index,
+                 std::size_t right_index) {
+    if (lazy[start_index] != 0) {
+      // The node in position start_index was marked as lazy
+      structure[start_index] += lazy[start_index];
+      if (left_index != right_index) {
+        auto left_child = left_child_index(start_index);
+        auto right_child = right_child_index(start_index);
+        lazy[left_child] += lazy[start_index];
+        lazy[right_child] += lazy[start_index];
+      } else {
+        // left_index = right_index is the time to update the origin array
+        origin[left_index] += lazy[start_index];
+      }
+      // mark as the node as not lazy
+      lazy[start_index] = 0;
+    }
+  }
+
+  std::size_t left_child_index(const std::size_t index) { return index * 2; }
+
+  std::size_t right_child_index(const std::size_t index) {
+    return (index * 2) + 1;
+  }
+
+ public:
+  LazySegmentTree(std::vector<T> &origin) : origin(origin) {
+    std::size_t size = origin.size();
+    structure = std::vector<T>(size * 4);
+    lazy = std::vector<T>(size * 4);
+    origin = origin;
+    build_structure(0, size);
+  }
+
+  virtual ~LazySegmentTree() {
+    structure.clear();
+    lazy.clear();
+  }
+
+  T range_query(std::size_t start_index, std::size_t end_index) {
+    return range_query_subroutine(1, 0, origin.size() - 1,
+                                  start_index, end_index);
+  }
+
+  /**
+   * This procedure update the value stored inside the structure in one single
+   * position, this use the update_range_subroutine with from == to == at; to
+   * run the procedure update
+   * @param at: it is the position in the original array, the function change
+   * the value also in the original array
+   * @param new_value the value that we want override in position at.
+   */
+  void update(std::size_t at, T value) {
+    update_range_subroutine(1, 0, origin.size() - 1, at, at, value);
+  }
+
+  /**
+   * Update the range with with the sum operation to the original array,
+   * This function will modify the original array original[pos] += new_val.
+   * @param from: Start position where start to update the original array.
+   * @param to: End position where stop to update the original array.
+   * @param new_val: The value to sum to each position of the range in the
+   * original array.
+   */
+  void update_range(std::size_t from, std::size_t to, T new_val) {
+    update_range_subroutine(1, 0, origin.size() - 1, from, to, new_val);
+  }
+};
+};  // namespace cpstl
+// Last update 9/1/2021
+
+struct Query {
+  /**
+   * 1. Increase the first value in range [a,b] by 1, the second value by 2, the third value by 3, and so on.
+   * 2. Calculate the sum of values in range [a,b].
+   */
+  uint type;
+  std::size_t start;
+  std::size_t end;
+  Query(uint type, size_t start, size_t anEnd)
+      : type(type), start(start - 1), end(anEnd - 1) {}
+};
+
+template <typename T, typename R>
+static std::vector<R> exponential_sum_in_a_range(std::vector<T> &inputs,
+                                                 std::vector<Query> const &queries) {
+  std::vector<R> results;
+  results.reserve(queries.size());  // over estimation
+  auto segment_tree = cpstl::LazySegmentTree<T>(inputs);
+  for (auto &query : queries) {
+    if (query.type == 1) {
+      T counter = 1;
+      for (std::size_t i = query.start; i <= query.end; i++) {
+        segment_tree.update(i, counter);
+        counter++;
+      }
+    } else {
+      auto value = segment_tree.range_query(query.start, query.end);
+      results.push_back(value);
+    }
+  }
+  return results;
+}
+
+int main() {
+  uint N, Q;
+  scanf("%d%d", &N, &Q);
+
+  std::vector<long long> inputs;
+  inputs.reserve(N);
+
+  for (std::size_t i = 0; i < N; i++) {
+    long long value;
+    scanf("%lld", &value);
+    inputs.push_back(value);
+  }
+
+  std::vector<Query> queries;
+  queries.reserve(Q);
+  for (std::size_t t = 0; t < Q; t++) {
+    uint type;
+    std::size_t start, end;
+    cin >> type;
+    cin >> start;
+    cin >> end;
+    queries.emplace_back(type, start, end);
+  }
+
+  auto results =
+      exponential_sum_in_a_range<long long, long long>(inputs, queries);
+  for (auto result : results) printf("%lld\n", result);
+}
