08c4e19d68a74d890ac38e85ad67bf1c654da21c
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-03-10 18:50:12 +0100

    Working in progress :construction:
    This commit add the Reply Challenge but it is still under developing.
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ Appendix/reply/ReplyWorkers/src/core/ReplyWorkers.hpp
@@ -0,0 +1,61 @@
+/**
+ * Competitive-Programming-and-Contests-VP-Solution a collection of
+ * code with an engineering approach to solve the problem.
+ * https://github.com/vincenzopalazzo/Competitive-Programming-and-Contests-VP-Solution
+ *
+ * Copyright (C) 2020-2021  Vincenzo Palazzo vincenzopalazzodev@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+#include "../test/Utils.hpp"
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+const cpstl::Log LOG(true);
+
+template<typename T>
+struct Developer {
+  std::string company;
+  T bonus;
+  T skill_factor;
+  std::vector<std::string> skills;
+
+  Developer(std::string &company, T bonus, T skill_factor, std::vector<std::string> skills):
+    company(company), bonus(bonus), skill_factor(skill_factor), skills(skills) {}
+};
+
+template<typename T>
+struct Manager {
+  std::string company;
+  T bonus;
+
+  Manager(std::string company, T bonus): company(company), bonus(bonus) {}
+};
+
+template<typename T>
+struct OfficeFloor {
+  T width;
+  T hight;
+  // TODO: Finish this way!
+  // Mapping the free position here from the map
+};
+
+// write the solution, but before I need to set understand what it the best way to
+// to manage the data. For the moment I'm thinking to work with the followng data
+// <OfficeFloor, Manager(s), Developer(s)> but the only problem that I have node is to
+// if the OfficeFloor contains also the developer inside and we have the data about that.
+template <typename T> static void solution(std::vector<T> const &inputs) {}
---
+++ Appendix/reply/ReplyWorkers/src/test/Utils.hpp
@@ -0,0 +1,139 @@
+/**
+ * CPSTL Utils a collection of method to make common operation with C++
+ * Copyright (C) 2020  Vincenzo Palazzo vincenzopalazzodev@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+#include "colors.h"
+
+using namespace std;
+
+namespace cpstl {
+
+    struct Log {
+        bool enable = false;
+
+        Log(bool enable) : enable(enable) {}
+    };
+
+    const cpstl::Log INTERNAL_LOG(false);
+
+    inline void cp_log(Log log, std::string message)
+    {
+        if (!log.enable) return;
+        std::cout << BOLDYELLOW;
+        std::cout << message;
+        std::cout << "\n";
+        std::cout << RESET;
+    }
+
+    template<typename T>
+    inline void cp_log(Log log, std::vector<T> const &inputs)
+    {
+        if (!log.enable) return;
+        std::cout << BOLDYELLOW;
+        print_vector(inputs);
+        std::cout << "\n";
+        std::cout << RESET;
+    }
+
+    template<typename T>
+    inline void print_vector(std::vector<T> const &inputs)
+    {
+        std::cout << BOLDYELLOW;
+        std::cout << "Vector: [";
+        for (int i = 0; i < inputs.size(); i++) {
+            if (i == inputs.size() - 1) {
+                std::cout << inputs[i] << "]";
+                continue;
+            }
+            cout << inputs[i] << ", ";
+        }
+        cout << "\n";
+        cout << RESET;
+    }
+
+    /**
+     * This procedure is wrote to load data from the test data
+     * download from the https://www.hackerrank.com/
+     * @tparam T type of vector
+     * @return return a single vector.
+     */
+    template<typename T>
+    inline std::vector<T> load_input_from_string(std::string &input_string)
+    {
+        string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+            return x == y and x == ' ';
+        });
+
+        input_string.erase(new_end, input_string.end());
+
+        while (input_string[input_string.length() - 1] == ' ') {
+            input_string.pop_back();
+        }
+
+        vector<string> splits;
+        char delimiter = ' ';
+
+        size_t i = 0;
+        size_t pos = input_string.find(delimiter);
+
+        while (pos != string::npos) {
+            splits.push_back(input_string.substr(i, pos - i));
+
+            i = pos + 1;
+            pos = input_string.find(delimiter, i);
+        }
+
+        splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+        return splits;
+    }
+
+
+  std::vector<std::string> tokenize_input_into_strings(std::string const &input_path)
+  {
+    std::ifstream input_file(input_path);
+    assert(input_file.is_open() == true);
+    std::string line;
+    std::vector<std::string> lines;
+    while(getline(input_file, line)) {
+      lines.push_back(line);
+      cp_log(INTERNAL_LOG, line);
+    }
+    return lines;
+  }
+
+
+
+  std::vector<std::string> tokenize_string_by_simbol(std::string &line, char symbol)
+  {
+    std::vector<std::string> tokens;
+    std::string token;
+    for (auto elem : line) {
+      if (elem == symbol) {
+        tokens.push_back(token);
+        cp_log(INTERNAL_LOG, "Push token: " + token);
+        token = "";
+      } else {
+        token += elem;
+      }
+    }
+    return tokens;
+  }
+}
