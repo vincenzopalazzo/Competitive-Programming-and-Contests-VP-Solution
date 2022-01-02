a2fee39231d1b2dbdaaf084a3cd6beb2afa39a47
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-01-02 22:50:53 +0100

    starting google kikstart 2021 with golang
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ Appendix/google_kikstart/2021/go.mod
@@ -0,0 +1,3 @@
+module github.com/vincenzopalazzo/Competitive-Programming-and-Contests-VP-Solution/Appendix/google_kikstart/2021
+
+go 1.17
---
+++ Appendix/google_kikstart/2021/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	round_a "github.com/vincenzopalazzo/Competitive-Programming-and-Contests-VP-Solution/Appendix/google_kikstart/2021/round_a"
+)
+
+func init() {
+
+}
+
+func main() {
+	round_a.SolveLShapedPlots()
+}
